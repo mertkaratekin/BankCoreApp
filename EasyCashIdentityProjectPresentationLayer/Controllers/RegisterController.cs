a73d29ce0a518df87e8e26cb6ad5dcbd8993967f
Mert Karatekin <mert1616karatekin@gmail.com>
2023-07-14 11:07:24 +0300

    Ders 11 sonu. Register controller eklendi.

---
+++ EasyCashIdentityProjectPresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,53 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProjectPresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = appUserRegisterDto.Username,
+                    Name = appUserRegisterDto.Name,
+                    Surname= appUserRegisterDto.Surname,
+                    Email= appUserRegisterDto.Email,
+                    City="aaaa",
+                    District="bbbb",
+                    ImageUrl="cccc"
+                };
+                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","ConfirmMail");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)  //resultan gelen hataları okumak için
+                    {
+                        ModelState.AddModelError("", item.Description); // hataları modelstate in içine eklemek için
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
