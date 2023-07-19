6a8fe681bef79960508a85d81ef729595ce0d9ed
Mert Karatekin <mert1616karatekin@gmail.com>
2023-07-19 11:26:00 +0300

    26. Ders Sonu

---
+++ EasyCashIdentityProjectDataAccessLayer/Migrations/20230719074016_mig_CustomerAccountProcess_add_description.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashIdentityProjectDataAccessLayer.Migrations
+{
+    public partial class mig_CustomerAccountProcess_add_description : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CustomerAccountId",
+                table: "CustomerAccounts",
+                newName: "CustomerAccountID");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CustomerAccountProcesses",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerAccountID",
+                table: "CustomerAccounts",
+                newName: "CustomerAccountId");
+        }
+    }
+}
